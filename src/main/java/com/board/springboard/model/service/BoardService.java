9a1008c953eb31b08a77a4ec04fa28f3c6b76fc4
so27kk <so27kk@gmail.com>
2026-03-09 12:54:33 +0900

    board 이미지 업로드

--- src/main/java/com/board/springboard/model/service/BoardService.java
+++ src/main/java/com/board/springboard/model/service/BoardService.java
@@ -1,44 +1,76 @@
 package com.board.springboard.model.service;
 
+
 import com.board.springboard.model.dto.Board;
 import com.board.springboard.model.mapper.BoardMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor // new 자바객체파일() 생략
 public class BoardService {
     // 보드 매퍼 에 작성된 기능을 활용하겠다.
-    // 보드 매퍼 는 xml에서 가져온 SQL 기능을 보유하고 있는 명칭들의 집합소
+    // 보드 매퍼 는 xml에서 가져온 SQL 기능을 보유하고 있는 명칭들의 집합소!
     private final BoardMapper boardMapper;
+
+    @Value("${file.upload.board-path}")
+    private String boardUploadPath; // application.yml -> config.yml 에서 가져오는 경로
 
     // 전체게시물 조회
     public List<Board> findAllBoard(){
-       return boardMapper.전체게시물();
+        return boardMapper.전체게시물();
     }
 
     // 게시물 하나 조회 + 조회수 증가 추가
     public Board boardDetail(int board_no){
         // 게시물을 불러오기 전에 조회수증가된 것을 수정하고
         boardMapper.조회수수정(board_no);
-        // 소비자에게 소비자가 클릭한 게시물 데이터를 SQL 가져와서 화면에 전달한다
+        // 소비자에게 소비자가 클릭한 게시물 데이터를 SQL 가져와서 화면에 전달한다.
         return boardMapper.단일게시물(board_no);
     }
 
-    // 게시물 추가 됐는지 유뮤만 확인
-    public void writeBoard(Board board){
+    // 게시물 추가 됐는지 유무만 확인
+    public void writeBoard(Board board, MultipartFile imageFile, String uploadPath) throws IOException {
+        // TODO 1 : 만약 이미지 파일이 있을 경우에만 컬럼 데이터 업로드
+        if(imageFile != null & !imageFile.isEmpty()) {
+            // TODO 2 : 회사컴퓨터 or 개발자컴퓨터에 저장 폴더 없으면 자동 생성
+            File folder = new File(boardUploadPath);
+            if (!folder.exists()) folder.mkdirs();
+
+            // TODO 3 : UUID + 원본 확장자로 파일명 생성 (중복파일명 충돌 방지)
+            String 원본파일이름 = imageFile.getOriginalFilename();
+            String 확장자 = 원본파일이름.substring(원본파일이름.lastIndexOf("."));
+            String 저장할파일 = UUID.randomUUID().toString()+확장자;
+
+            // TODO 4 : 회사컴퓨터 or 개발자컴퓨터에 실제 클라이언트가 전달한 파일 저장
+            File 파일저장 = new File(boardUploadPath + "/" + 저장할파일);
+            imageFile.transferTo(파일저장);
+
+            // TODO 5 : DB에 저장할 웹 접근 경로를 Board 객체 세팅
+            String 웹에서_접근할_경로 = "/board/" + 저장할파일;
+            // String 웹에서_접근할_경로 = "/board" + 저장할파일;
+            // board파일이름랜덤.png 로 board가 파일이름 앞에 붙는 신세가 된다.
+            board.setAttach_img(웹에서_접근할_경로);
+        }
+        // TODO 6 : DB에 게시물 저장 (이미지 없으면 attach_img = null 로 저장되며, 수정할 일 없음)
         boardMapper.게시물추가(board);
+
     }
 
     // 게시물 수정 유무 나중에 확인
-    public void updateBoard(Board board){
+    public void updateBoard(Board board) {
         boardMapper.게시물수정(board);
     }
 
-    // 게시물 삭제 유뮤 나중에 확인
-    public void deleteBoard(int board_no){
+    // 게시물 삭제 유무 나중에 확인
+    public void  deleteBoard(int board_no) {
         boardMapper.게시물삭제(board_no);
     }
 }
