a4937e59f07383030c24f0646b9dde31cc3fa952
so27kk <so27kk@gmail.com>
2026-03-16 13:06:09 +0900

    JWt 토큰 시간

--- src/main/java/com/board/springboard/common/SecurityConfig.java
+++ src/main/java/com/board/springboard/common/SecurityConfig.java
@@ -1,14 +1,15 @@
 package com.board.springboard.common;
 
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-@EnableWebFluxSecurity
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final JwtProvider jwtProvider;
+    // 파일 같은 경우 -> common 에서 수제 제작해서 사용할 것
+    // private final JwtProvider jwtProvider;
 
 }
